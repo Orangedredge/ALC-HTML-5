4305b441d44ec4265eee38cbd903224a68977ac1
Unknown <mckay.r456@stu.nebo.edu>
2019-01-23 10:27:46 -0700

    starting the begging of are project

---
+++ Text based game/game.js
@@ -0,0 +1,18 @@
+// single line comment
+
+/*
+multiline comment
+*/
+
+
+
+//document.write("<h1>I cant wait for <em>spring</em>!</h1>");
+
+//alert("warning! will robinson... Warning!");
+
+//confirm("do you like pokemon?");
+
+//prompt("what type of pokemon do you like?");
+var pc = prompt("what is your name");
+confirm("so your name is " + pc);
+confirm("you wake up in a tunel ")
