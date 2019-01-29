f84960f6c62433123c834d6e1cfb89264256204f
Unknown <mckay.r456@stu.nebo.edu>
2019-01-29 10:29:26 -0700

    huge update with some what working code

---
+++ Text based game/in class coding/java.js
@@ -0,0 +1,318 @@
+//alert('test')
+var x = 0;
+var y = 0;
+var px = 0;
+var py = 0;
+var dev = 0;
+//iteam variables which store the iteams location
+//sword
+var zword = false
+var swordx = 0;
+var swordy = -1;
+
+
+//book
+var bookx = -2;
+var booky = 1;
+var book = false;
+
+//combat related sutff like health and magic that aplly to the player
+var health = 20;
+var magic = 20;
+var power = 5;
+var level = 1;
+
+//these are combat related varables
+var turnorder = 0;
+
+
+
+
+
+
+var invintory = ["nothing","nothing","nothing","nothing","nothing","nothing"]
+function check(x,y){
+	//after a location update this function ask and updates your location
+	var check = (prompt("what would you like to do"))
+	//basic movment
+	if (check == "north" || check == "n"){
+		y += 1;
+		Lc(x,y);
+	}else if(check == "w" || check == "west"){
+		x -= 1;
+		Lc(x,y);
+		
+	}else if(check == "south" || check == "s"){
+		y -= 1;
+		Lc(x,y);
+	}else if(check == "east" || check == "e"){
+		x += 1;
+		Lc(x,y);
+	}else if(check == "inventory" || check == "iteams"){
+		var i
+		for (i = 0; i < 6; i++) { 
+			alert(invintory[i])
+		  }
+		Lc(x,y);
+	
+	}else if(check == "end"){
+		alert("game ended");
+	
+
+
+
+
+	//more advanced checks
+	}else if(check == "devoption"){
+		dev = 1;
+		Lc(x,y);
+	}else if (check == "set xy" && dev == 1){
+		x = prompt("what will x be");
+		y = prompt("what will y be");
+		Lc(x,y)
+	//iteam pick up
+}else if (check == "pick up sword" && swordx == x && swordy == y){
+	var i;
+	var j;
+	for (i = 0;i <6; ++i){
+		if (invintory[i] == "sword"){
+			alert("you allready have the sword");
+			zword =true;
+			i += 6;
+			Lc(x,y);
+			
+		}else{
+			zword = false;
+		}
+			
+		alert("this is a test")
+		}
+		alert("this is a test2")
+	if (zword == false){
+		var i = 0;
+		for (i = 0; i <6; ++i){
+			alert("this is test 3")
+			if (invintory[i] == "nothing"){
+				alert("this is test 4")
+				invintory[i] = "sword"
+				i += 6
+				zword = true;
+				
+				alert("you picked up the sword")
+				Lc(x,y);
+				alert("test 6")
+			}else{
+				
+				
+			}
+		}
+	}
+
+
+	}else if (check == "pick up book" && bookx == x && booky == y){
+		var i;
+		var j;
+		for (i = 0;i <6; ++i){
+			if (invintory[i] == "book"){
+				alert("you allready have the book");
+				book =true;
+				i += 6;
+				Lc(x,y);
+				
+			}else{
+				book = false;
+			}
+				
+			alert("this is a test")
+			}
+			alert("this is a test2")
+		if (book == false){
+			var i = 0;
+			for (i = 0; i <7; ++i){
+				alert("this is test 3")
+				if (i == 6 ){
+					alert("not enough room")
+				}
+				if (invintory[i] == "nothing"){
+					alert("this is test 4")
+					invintory[i] = "book"
+					i += 6
+					
+					alert("you picked up the book")
+					Lc(x,y);
+					alert("test 6")
+				}else{
+					//alert("you have no space");
+					//Lc(x,y);
+					//alert ("test 5")
+				}
+			}
+		}
+		
+	
+	
+	
+	check(x,y);
+	}else{
+		alert("you cant do that");
+		Lc(x,y);
+	}
+
+}
+// this functino is used for checking location
+function Lc(x,y){
+	if (dev == 1){
+		alert(x);
+		alert(y);
+
+	}
+	
+
+	//this is area that checks your loctaion
+	// This is the cave area
+	if (x == 0 && y == 0){
+		px = x;
+		py = y;
+		alert("you are in the cave and can go east west and south also there may be an exit to your east")
+		check(x,y);
+
+
+
+	}else if(x == -1 && y == 0){
+		px = x;
+		py = y;
+		alert("you are in the cave and can go east, west also there may be an exit to your east")
+		check(x,y);
+
+
+
+	}else if(x == 0 && y == -1){
+		px = x;
+		py = y;
+		if (zword == false){
+			alert("you enter a small area with a rusted sword on the ground and may move to the north");
+		
+		}else{
+			alert("you enter a small area and may move to the north");
+		}
+
+		check(x,y);
+
+
+
+
+	}else if(x == 1 && y == 0){
+		px = x;
+		py = y;
+		alert("getting closer to exit you may move east and west")
+		check(x,y);
+
+
+
+
+	}else if(x == -2 && y == 0){
+		px = x;
+		py = y;
+		alert("you  are able to move east and north")
+		check(x,y);
+	}else if(x == -2 && y == 1){
+		px = x;
+		py = y;
+		
+		var i;
+		for (i = 0; i < 6; i++) { 
+			if (invintory[i] == "book"){
+				alert("nothing to see in here it is a dead end you may go south");
+				check(x,y);
+
+			}else{
+				alert("it is dead end but there is a book lying on the ground ");
+				check(x,y);
+
+			}
+				
+			  
+		
+	}
+		
+	}else if(x == 2 && y == 0){
+		px = x;
+		py = y;
+		alert("a group of slimes come out to attack you   ")
+		check(x,y);
+	}else if(x == -1 && y == 0){
+		px = x;
+		py = y;
+		alert("");
+		check(x,y);
+	}
+	
+		
+	
+
+
+
+
+
+
+
+
+
+	
+	alert("Can not do that command");
+	x = px;
+	y = py;
+	Lc(x,y);
+	
+}
+
+
+
+
+
+
+
+
+
+
+var enemey= false;
+var slimehealth = 10;
+var slimeattack = 15;
+
+function combatcheck(enemeyhealth){
+	var combatcheck = (promp("what would you like to do"))
+	if (combatcheck == "attack"){
+		slimehealth -= (attack * (level * 1.25));
+		turnorder = 1;
+		combat(slime)
+	}
+
+}
+
+
+function combat(enemey){
+	if (turnorder == 0){
+		combatcheck(slimehealth);
+	}else if (enemey = slime){
+		var slimerandom = Math.floor(Math.random() * 3);
+		if (slimerandom <= 1){
+			
+		}
+		
+
+	}
+	
+}
+
+
+
+
+
+
+if (confirm("Would you like to \nplay the game")) {
+	alert("you wake up in a cave,\n you have no memory and nothing but cloth on \n you can feel wind coming to your east but it might be good to explore some more \n your opion are east/e, west/w, south/s,north/n, inventory/iteam");
+    Lc(x,y);
+  } else {
+    alert('OK \ if you wish to play again');
+  }
+
